a3259eb62259d0155eef29530dadecefb4c98551
richard4s <richardoluwo50@gmail.com>
2020-02-14 08:21:49 +0100

    Fixed navbar vertically

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import { Image, Grid, Menu } from 'semantic-ui-react'
+// import { Image, Grid, Menu } from 'semantic-ui-react'
 
-// import {Logo} from '../assets/images/kliine-default.png'
-import Logo from '../assets/images/kliine-default.png'
+import Navbar from '../layouts/navbar'
 
 export default class Dashboard extends Component {
     constructor(props) {
@@ -17,53 +16,12 @@ export default class Dashboard extends Component {
 
     render() {
         
-        const { activeItem } = this.state
+        // const { activeItem } = this.state
 
         return(
-            <Menu secondary>
-
-                <Grid>
-                    <Grid.Row>
-
-                        <Grid.Column width={6}>
-                            <Menu.Item
-                                name='home'
-                                active={activeItem === 'home'}
-                                onClick={this.handleItemClick}
-                                >
-                                    <Image
-                                        alt='Kliine Logo'
-                                        size='medium'
-                                        loading="lazy"
-                                        src={Logo}
-                                        srcSet={Logo + ' 2x'} 
-                                    />
-                                </Menu.Item>
-                        </Grid.Column>
-
-                        <Grid.Column width={3}>
-                            <Menu.Item
-                                name='account'
-                                active={activeItem === 'account'}
-                                onClick={this.handleItemClick}
-                            >
-                                <p>Account Overview</p>   
-                            </Menu.Item>
-                        </Grid.Column>
-
-                        <Grid.Column width={3}>
-                            <Menu.Menu position='right'>
-                                <Menu.Item
-                                    name='logout'
-                                    active={activeItem === 'logout'}
-                                    onClick={this.handleItemClick}
-                                />
-                            </Menu.Menu>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-
-            </Menu>
+            <>
+                <Navbar />
+            </>
         )
     }
 }
